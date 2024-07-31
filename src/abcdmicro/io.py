d749bb29a9734f71a4cc49ffe5f63016ef116a2e
Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>
2024-07-31 10:50:58 -0400

    Add placeholders for IO setup (#35)

---
+++ src/abcdmicro/io.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from abcdmicro.resource import BvalResource, BvecResource, VolumeResource
+
+
+class NiftiVolumeResrouce(VolumeResource):
+    pass
+
+
+class FslBvalResource(BvalResource):
+    pass
+
+
+class FslBvecResource(BvecResource):
+    pass
