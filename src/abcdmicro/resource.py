d66887441aa8b248192ca7f331db365bcd66e9e8
Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>
2024-08-01 09:37:10 -0400

    Add class docstrings to Resource placeholders

--- src/abcdmicro/resource.py
+++ src/abcdmicro/resource.py
@@ -2,28 +2,34 @@ from __future__ import annotations
 
 
 class Resource:
-    pass
+    """Base class for resources."""
 
 
 class VolumeResource(Resource):
-    pass
+    """Base class for resources representing a volume or volume stack.
+    An n-D array where n >= 3 and where three of the dimensions are spatial
+    and have associated header information describing a patient coordinate system."""
 
 
 class InMemoryVolumeResource(VolumeResource):
-    pass
+    """A volume resource that is loaded into memory.
+    An n-D array where n >= 3 and where three of the dimensions are spatial
+    and have associated header information describing a patient coordinate system."""
 
 
 class BvalResource(Resource):
-    pass
+    """Base class for resources representing a list of b-values associated with a 4D DWI
+    volume stack."""
 
 
 class InMemoryBvalResource(BvalResource):
-    pass
+    """A b-value list that is loaded into memory."""
 
 
 class BvecResource(Resource):
-    pass
+    """Base class for resources representing a list of b-vectors associated with a 4D DWI
+    volume stack."""
 
 
 class InMemoryBvecResource(BvecResource):
-    pass
+    """A b-vector list that is loaded into memory."""
