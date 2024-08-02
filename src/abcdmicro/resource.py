1a5da5cd4efb7f16551db37d641c400a2de63a45
Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>
2024-08-02 12:34:22 -0400

    Implement bval and bvec in-memory resources (#35)

--- src/abcdmicro/resource.py
+++ src/abcdmicro/resource.py
@@ -1,14 +1,20 @@
 from __future__ import annotations
 
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
 
-class Resource:
-    """Base class for resources."""
+import numpy as np
+from numpy.typing import NDArray
 
 
-class VolumeResource(Resource):
+class VolumeResource(ABC):
     """Base class for resources representing a volume or volume stack.
     An n-D array where n >= 3 and where three of the dimensions are spatial
     and have associated header information describing a patient coordinate system."""
+
+    @abstractmethod
+    def get_array(self) -> NDArray[np.floating]:
+        """Get the underlying volume data array"""
 
 
 class InMemoryVolumeResource(VolumeResource):
@@ -17,19 +23,41 @@ class InMemoryVolumeResource(VolumeResource):
     and have associated header information describing a patient coordinate system."""
 
 
-class BvalResource(Resource):
+class BvalResource(ABC):
     """Base class for resources representing a list of b-values associated with a 4D DWI
     volume stack."""
 
+    @abstractmethod
+    def get(self) -> NDArray[np.floating]:
+        """Get the underlying array of b-values"""
 
+
+@dataclass
 class InMemoryBvalResource(BvalResource):
     """A b-value list that is loaded into memory."""
 
+    array: NDArray[np.floating]
+    """The underlying array of b-values"""
 
-class BvecResource(Resource):
+    def get(self) -> NDArray[np.floating]:
+        return self.array
+
+
+class BvecResource(ABC):
     """Base class for resources representing a list of b-vectors associated with a 4D DWI
     volume stack."""
 
+    @abstractmethod
+    def get(self) -> NDArray[np.floating]:
+        """Get the underlying array of b-vectors"""
 
+
+@dataclass
 class InMemoryBvecResource(BvecResource):
     """A b-vector list that is loaded into memory."""
+
+    array: NDArray[np.floating]
+    """The underlying array of b-vectors"""
+
+    def get(self) -> NDArray[np.floating]:
+        return self.array
