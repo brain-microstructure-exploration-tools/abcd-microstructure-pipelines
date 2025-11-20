4defe6f6e6e559dee4cbe827bf1cebbdeb2b5539
Sadhana Ravikumar <sadhana.ravikumar@kitware.com>
2025-11-20 08:53:41 -0500

    Update response function format to use sh coeffs

--- src/abcdmicro/resource.py
+++ src/abcdmicro/resource.py
@@ -5,7 +5,15 @@ from dataclasses import dataclass, field
 from typing import Any, ClassVar
 
 import numpy as np
-from numpy.typing import NDArray
+from dipy.core.geometry import cart2sphere
+from dipy.core.gradients import GradientTable
+from dipy.reconst.csdeconv import AxSymShResponse, estimate_response
+from dipy.reconst.shm import (
+    lazy_index,
+    real_sh_descoteaux_from_index,
+    sph_harm_ind_list,
+)
+from numpy.typing import NDArray, Tuple
 
 
 class Resource(ABC):
@@ -126,6 +134,10 @@ class ResponseFunctionResource(Resource):
     def get(self) -> tuple[NDArray, np.floating]:
         """Get the underlying response function"""
 
+    @abstractmethod
+    def get_dipy_object(self) -> AxSymShResponse:
+        """Get the underlying response function in a format compatible with Dipy"""
+
     def load(self) -> ResponseFunctionResource:
         return self
 
@@ -136,17 +148,74 @@ class InMemoryResponseFunctionResource(ResponseFunctionResource):
 
     is_loaded: ClassVar[bool] = True
 
-    evals: NDArray[np.floating]
-    """Eigenvales of the response function"""
+    sh_coeffs: NDArray[np.floating]
+    """Response function signal as coefficients to an axially symmetric, even spherical harmonic model."""
 
     avg_signal: np.floating
     """ The average non-diffusion weighted signal within the voxels used to calculate the response function"""
 
-    def __post_init__(self) -> None:
-        # Check that the eigen values have the expected shape
-        if self.evals.ndim != 1 or self.evals.shape[0] != 3:
-            msg = f"Encountered wrong eigen values array shape {self.evals.shape}. Expected shape (3,)."
-            raise ValueError(msg)
-
     def get(self) -> tuple[NDArray, np.floating]:
-        return (self.evals, self.avg_signal)
+        """Returns the underlying response function as (sh_coeffs, avg_signal)"""
+        return (self.sh_coeffs, self.avg_signal)
+
+    @staticmethod
+    def estimate_from_prolate_tensor(
+        response: Tuple[NDArray, np.floating],
+        gtab: GradientTable,
+        sh_order_max: int = 8,
+    ) -> InMemoryResponseFunctionResource:
+        """Convert a legacy DIPY prolate-tensor response `(evals, S0)` into
+        spherical harmonic coefficients using the approach from DIPY's
+        `csdeconv` module:
+        https://github.com/dipy/dipy/blob/f7b863f1485cd3fa6329c8e8f3388d8f58863f0d/dipy/reconst/csdeconv.py#L168.
+        Args:
+            response: Response function output by DIPY.
+            gtab: Gradient table used to estimate response
+            sh_order_max: Maximum spherical harmonic order to use for the basis model. Default is 8.
+        Returns: InMemoryResponseFunctionResource
+        """
+
+        if not isinstance(response, tuple) or len(response) != 2:
+            error_msg = "response must be a tuple of (eigen_values, avg_S0)."
+            raise ValueError(error_msg)
+
+        evals, s0 = response
+
+        if evals.ndim != 1 and evals.shape[0] != 3:
+            error_msg = "Encountered wrong eigen values array shape {self.evals.shape}. Expected shape (3,)"
+            raise ValueError(error_msg)
+
+        if gtab is None or not hasattr(gtab, "gradients"):
+            error_msg = "Invalid GradientTable provided."
+            raise ValueError(error_msg)
+
+        m_values, l_values = sph_harm_ind_list(sh_order_max)
+        _where_dwi = lazy_index(~gtab.b0s_mask)
+
+        x, y, z = gtab.gradients[_where_dwi].T
+        _, theta, phi = cart2sphere(x, y, z)
+        B_dwi = real_sh_descoteaux_from_index(
+            m_values, l_values, theta[:, None], phi[:, None]
+        )
+
+        S_r = estimate_response(gtab, evals, s0)
+        sh_coeffs = np.linalg.lstsq(B_dwi, S_r[_where_dwi], rcond=-1)[0]
+
+        return InMemoryResponseFunctionResource(sh_coeffs=sh_coeffs, avg_signal=s0)
+
+    @staticmethod
+    def from_dipy_object(obj: AxSymShResponse) -> InMemoryResponseFunctionResource:
+        """Construct from a DIPY `AxSymShResponse` instance."""
+
+        if not isinstance(obj, AxSymShResponse):
+            error_msg = "from_dipy_object requires an AxSymShResponse instance."
+            raise TypeError(error_msg)
+
+        return InMemoryResponseFunctionResource(
+            sh_coeffs=obj.dwi_response, avg_signal=obj.S0
+        )
+
+    def get_dipy_object(self) -> AxSymShResponse:
+        """Return the stored response function as a DIPY `AxSymShResponse`."""
+
+        return AxSymShResponse(S0=self.avg_signal, dwi_response=self.sh_coeffs)
